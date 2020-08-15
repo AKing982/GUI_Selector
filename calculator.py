f18455a53320930828e926797d477149be070a5a
Alexander King <alex@utahkings.com>
2020-08-15 16:56:20 -0600

    modified code in calculator.py

--- calculator.py
+++ calculator.py
@@ -4,3 +4,13 @@ class CalcGUI:
     def __init__(self):
         # Create the main window for our calculator app
         self.main_window = Tk()
+        self.main_window.geometry('500x200')
+        self.main_window.title('Calculator')
+
+        # Create the Five frames
+        self.entry_frame = Frame(self.main_window)
+        self.btn_row1_frame = Frame(self.main_window)
+        self.btn_row2_frame = Frame(self.main_window)
+        self.btn_row3_frame = Frame(self.main_window)
+        self.btn_row4_frame = Frame(self.main_window)
+
